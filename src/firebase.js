4ac959fc747008b536435cf40082c9095e1a9bce
Dhruv Kanetkar <kanetkard@gmail.com>
2023-10-07 10:24:45 -0700

    base firebase config

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAIYATBu6Dzreswmx7HfUvg6BlIjKdeU4o",
+  authDomain: "magellan-62c66.firebaseapp.com",
+  projectId: "magellan-62c66",
+  storageBucket: "magellan-62c66.appspot.com",
+  messagingSenderId: "769493190655",
+  appId: "1:769493190655:web:a4adbce66a79a08f3049ca",
+  measurementId: "G-9HJKSD7L9W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

d7ed3cf9379e637448b04fd0d8285576d893a23d
Dhruv Kanetkar <kanetkard@gmail.com>
2023-10-08 09:58:27 -0700

    styling cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";

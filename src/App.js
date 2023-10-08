5c6e6e895cb5175c8bf8cb3380be37ca56737ec6
Dhruv Kanetkar <kanetkard@gmail.com>
2023-10-07 23:16:05 -0700

    app routing changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import './App.css';
-import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-
+import { Container, Row, Col } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import { UserAuthContextProvider } from "./utils/AuthContext";
 
 function App() {
   return (
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <Container style={{ width: "400px" }}>
+      <Row>
+        <Col>
+          <UserAuthContextProvider>
+            <Routes>
+              <Route
+                path="/home"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </UserAuthContextProvider>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 

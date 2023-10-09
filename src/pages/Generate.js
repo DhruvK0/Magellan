614402eb6f839df4b575e80d2f56a06347df6d20
Dhruv Kanetkar <kanetkard@gmail.com>
2023-10-08 23:21:42 -0700

    added json template

--- src/pages/Generate.js
+++ src/pages/Generate.js
@@ -1,102 +1,210 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk';
+import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike';
+import SportsBaseballIcon from '@mui/icons-material/SportsBaseball';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import NatureIcon from '@mui/icons-material/Nature';
+import LocalBarIcon from '@mui/icons-material/LocalBar';
 
-const parseItinerary = (itineraryText) => {
-  const lines = itineraryText.split('\n');
-  const itinerary = [];
-  let currentDay = null;
-
-  for (const line of lines) {
-    const trimmedLine = line.trim();
-
-    if (trimmedLine) {
-      if (trimmedLine.match(/^\d{4}-\d{2}-\d{2}:$/)) {
-        if (currentDay) {
-          itinerary.push(currentDay);
+const itineraryData = {
+  "itinerary": [
+    {
+      "date": "2023-08-10",
+      "activities": [
+        {
+          "activity": "Breakfast: The Mill",
+          "link": "https://www.themillsf.com/"
+        },
+        {
+          "activity": "Morning Activity: Hiking in Lands End",
+          "link": "https://www.nps.gov/goga/planyourvisit/landsend.htm"
+        },
+        {
+          "activity": "Lunch: The Yellow Submarine",
+          "link": "https://www.yellowsubmarine-sf.com/"
+        },
+        {
+          "activity": "Afternoon Activity: Golden Gate Park Segway Tour",
+          "link": "https://electrictourcompany.com/san-francisco-tours/golden-gate-park-segway-tour/"
+        },
+        {
+          "activity": "Dinner: Nopalito",
+          "link": "https://www.nopalitosf.com/"
         }
-        const [date] = trimmedLine.split(':');
-        currentDay = {
-          date: date.trim(),
-          activities: [],
-        };
-      } else if (currentDay) {
-        const [activity] = trimmedLine.split(':');
-        const linkMatch = trimmedLine.match(/\bhttps?:\/\/\S+/);
-        if (activity && linkMatch) {
-          const link = linkMatch[0];
-          currentDay.activities.push({ activity: activity.trim(), link });
+      ]
+    },
+    {
+      "date": "2023-08-11",
+      "activities": [
+        {
+          "activity": "Breakfast: Plow",
+          "link": "https://www.eatatplow.com/"
+        },
+        {
+          "activity": "Morning Activity: Exploratorium",
+          "link": "https://www.exploratorium.edu/"
+        },
+        {
+          "activity": "Lunch: The Codmother Fish and Chips",
+          "link": "https://www.codmother.com/"
+        },
+        {
+          "activity": "Afternoon Activity: Alcatraz Island Tour",
+          "link": "https://www.nps.gov/alca/index.htm"
+        },
+        {
+          "activity": "Dinner: Rich Table",
+          "link": "https://www.richtablesf.com/"
         }
-      }
+      ]
+    },
+    {
+      "date": "2023-08-12",
+      "activities": [
+        {
+          "activity": "Breakfast: Mama's on Washington Square",
+          "link": "https://www.mamas-sf.com/"
+        },
+        {
+          "activity": "Morning Activity: San Francisco Bike Tour",
+          "link": "https://www.blazingsaddles.com/san-francisco-bike-tours/"
+        },
+        {
+          "activity": "Lunch: Craftsman and Wolves",
+          "link": "https://www.craftsman-wolves.com/"
+        },
+        {
+          "activity": "Afternoon Activity: San Francisco Giants Game at AT&T Park",
+          "link": "https://www.mlb.com/giants"
+        },
+        {
+          "activity": "Dinner: Burma Superstar",
+          "link": "https://www.burmasuperstar.com/"
+        }
+      ]
+    },
+    {
+      "date": "2023-08-13",
+      "activities": [
+        {
+          "activity": "Breakfast: Tartine Bakery",
+          "link": "https://www.tartinebakery.com/"
+        },
+        {
+          "activity": "Morning Activity: Muir Woods National Monument",
+          "link": "https://www.nps.gov/muwo/index.htm"
+        },
+        {
+          "activity": "Lunch: Sol Food",
+          "link": "https://www.solfoodrestaurant.com/"
+        },
+        {
+          "activity": "Afternoon Activity: Wine Country Tour of Napa and Sonoma",
+          "link": "https://www.viator.com/tours/San-Francisco/Napa-and-Sonoma-Wine-Country-Tour/d651-2660SFOWIN"
+        },
+        {
+          "activity": "Dinner: Cotogna",
+          "link": "https://cotognasf.com/"
+        }
+      ]
+    },
+    {
+      "date": "2023-08-14",
+      "activities": [
+        {
+          "activity": "Breakfast: Sweet Maple",
+          "link": "https://www.sweetmaplesf.com/"
+        },
+        {
+          "activity": "Morning Activity: Ferry Building Marketplace",
+          "link": "https://www.ferrybuildingmarketplace.com/"
+        },
+        {
+          "activity": "Lunch: Hog Island Oyster Co.",
+          "link": "https://hogislandoysters.com/"
+        },
+        {
+          "activity": "Afternoon Activity: Cable Car Ride",
+          "link": "https://www.sfmta.com/getting-around/transit/how-ride/cable-cars"
+        },
+        {
+          "activity": "Dinner: Dos Piñas",
+          "link": "https://www.dospinas.com/"
+        }
+      ]
     }
-  }
+  ]
+}
 
-  if (currentDay) {
-    itinerary.push(currentDay);
-  }
 
-  return itinerary;
+const activityIcons = {
+  'Breakfast': <FastfoodIcon />,
+  'Morning Activity': <AccessTimeIcon />,
+  'Lunch': <RestaurantIcon />,
+  'Afternoon Activity': <DirectionsWalkIcon />,
+  'Dinner': <RestaurantIcon />,
+  'Bike Tour': <DirectionsBikeIcon />,
+  'Baseball Game': <SportsBaseballIcon />,
+  'Bakery': <FastfoodIcon />,
+  'Nature Tour': <NatureIcon />,
+  'Bar': <LocalBarIcon />,
 };
 
 const ItineraryTimeline = () => {
-   const text = "2023-08-10: \
-        Breakfast: The Mill - https://www.themillsf.com/ \
-        Morning Activity: Hiking in Lands End - https://www.nps.gov/goga/planyourvisit/landsend.htm \
-        Lunch: The Yellow Submarine - https://www.yellowsubmarine-sf.com/ \
-        Afternoon Activity: Golden Gate Park Segway Tour - https://electrictourcompany.com/san-francisco-tours/golden-gate-park-segway-tour/ \
-        Dinner: Nopalito - https://www.nopalitosf.com/ \
- \
-    2023-08-11: \
-        Breakfast: Plow - https://www.eatatplow.com/ \
-        Morning Activity: Exploratorium - https://www.exploratorium.edu/ \
-        Lunch: The Codmother Fish and Chips - https://www.codmother.com/ \
-        Afternoon Activity: Alcatraz Island Tour - https://www.nps.gov/alca/index.htm \
-        Dinner: Rich Table - https://www.richtablesf.com/ \
- \
-    2023-08-12: \
-        Breakfast: Mama's on Washington Square - https://www.mamas-sf.com/ \
-        Morning Activity: San Francisco Bike Tour - https://www.blazingsaddles.com/san-francisco-bike-tours/ \
-        Lunch: Craftsman and Wolves - https://www.craftsman-wolves.com/ \
-        Afternoon Activity: San Francisco Giants Game at AT&T Park - https://www.mlb.com/giants \
-        Dinner: Burma Superstar - https://www.burmasuperstar.com/ \
- \
-    2023-08-13: \
-        Breakfast: Tartine Bakery - https://www.tartinebakery.com/ \
-        Morning Activity: Muir Woods National Monument - https://www.nps.gov/muwo/index.htm \
-        Lunch: Sol Food - https://www.solfoodrestaurant.com/ \
-        Afternoon Activity: Wine Country Tour of Napa and Sonoma - https://www.viator.com/tours/San-Francisco/Napa-and-Sonoma-Wine-Country-Tour/d651-2660SFOWIN \
-        Dinner: Cotogna - https://cotognasf.com/ \
- \
-    2023-08-14: \
-        Breakfast: Sweet Maple - https://www.sweetmaplesf.com/ \
-        Morning Activity: Ferry Building Marketplace - https://www.ferrybuildingmarketplace.com/ \
-        Lunch: Hog Island Oyster Co. - https://hogislandoysters.com/ \
-        Afternoon Activity:  Cable Car Ride - https://www.sfmta.com/getting-around/transit/how-ride/cable-cars \
-        Dinner: Dos Piñas - https://www.dospinas.com/ \
-        ";
-  const itinerary = parseItinerary(text);
+  const itinerary = itineraryData.itinerary;
 
   return (
     <div className="p-4">
+        {/* Title */}
+      <Typography variant="h4" className="text-center mb-10">
+        Your Trip
+      </Typography>
+
       {itinerary.map((day, index) => (
-        <div key={index} className="mb-6">
-          <Typography variant="h5">{day.date}</Typography>
-          <ul className="list-disc ml-6">
-            {day.activities.map((activity, activityIndex) => (
-              <li key={activityIndex}>
-                {activity.activity}:{' '}
-                <a href={activity.link} target="_blank" rel="noopener noreferrer">
-                  {activity.link}
-                </a>
-              </li>
-            ))}
-          </ul>
+        <div
+          key={index}
+          className="mb-6 mx-auto max-w-screen-md" // Centered and 50% width
+        >
+          <Card>
+            <CardContent>
+              <Typography variant="h5" className="text-center">{day.date}</Typography>
+              <Stepper orientation="vertical" className="mt-4">
+                {day.activities.map((activity, activityIndex) => (
+                  <Step key={activityIndex}>
+                    <StepLabel>
+                      <div className="flex items-center">
+                        {activityIcons[activity.activity.split(':')[0]]}
+                        <Typography variant="body1" className="ml-2">
+                          {activity.activity.split(':')[1]}:{' '}
+                          <a href={activity.link} target="_blank" rel="noopener noreferrer">
+                            {activity.link}
+                          </a>
+                        </Typography>
+                      </div>
+                    </StepLabel>
+                  </Step>
+                ))}
+              </Stepper>
+            </CardContent>
+          </Card>
         </div>
       ))}
     </div>
   );
 };
 
+
 export default ItineraryTimeline;
+
 
 
 

7ce3739c8bdb41125f9253a06749c8e9b8fe20ec
Dhruv Kanetkar <kanetkard@gmail.com>
2024-01-10 15:54:28 -0800

    fixed dots

--- pwa/magellan/src/pages/Activities/TimelineWindow.js
+++ pwa/magellan/src/pages/Activities/TimelineWindow.js
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from 'react';
 
 
@@ -12,9 +13,13 @@ const TimelineWindow = ({ tripDates, itinerary,  addtimeline, setaddtimeline, se
 
     return (
       itinerary[date].map((activity, index) => (
-        <div className="flex flex-col items-center m-2">
-          <div className="w-4 h-4 bg-gray-600 rounded-full"></div>
-          <span className="text-xs">{activity.title}</span>
+        <div className="flex flex-col items-left m-2">
+          <div className='flex'>
+            <div>
+              <div className="w-3 h-3 bg-gray-600 rounded-full mr-2 mt-1"></div>
+            </div>
+            <span className="text-xs">{activity.title}</span>
+          </div>
         </div>
       ))
     );
@@ -33,7 +38,7 @@ const TimelineWindow = ({ tripDates, itinerary,  addtimeline, setaddtimeline, se
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{date}</time>
 
           {addtimeline && date === currentDate ?
-            <div>
+            <div className='w-64'>
               <button onClick={() => handleAddActivity(setaddtimeline, !addtimeline, setdate, "")} className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Currently Adding Activities
                 <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
@@ -43,7 +48,7 @@ const TimelineWindow = ({ tripDates, itinerary,  addtimeline, setaddtimeline, se
               
             </div> :
 
-            <div>
+            <div className='w-64'>
               <button onClick={() => handleAddActivity(setaddtimeline, !addtimeline, setdate, date)} className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Add Activity
                 <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
